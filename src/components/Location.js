ed8035d564a87fa016eed8e7f4b2b4dfc1323e77
Mandy Mason <mnmason86@gmail.com>
2022-07-19 15:02:07 -0400

    refactored getLocation function, began adding errors

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,7 +3,7 @@ import {Component} from 'react';
 import axios from 'axios';
 import Card from 'react-bootstrap/Card';
 
-const key = process.env.REACT_APP_CITY_KEY;
+const apiKey = process.env.REACT_APP_CITY_KEY;
 
 
 class Location extends Component {
@@ -11,33 +11,41 @@ class Location extends Component {
   constructor(props){
     super(props);
     this.state={
-      searchQuery:'',
-      location: {},
-      show: false
+      lat: 0.00,
+      lon: 0.00,
+      name: '',
+      error: { status: null, message: null}
     }
   }
 
-  getLocation = async () => {
-    const API = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${this.state.searchQuery}&format=json`;
-    const response = await axios.get(API);
-    console.log(response.data[0]);
-    this.setState({ location:response.data[0], show:true });
-  }
+  getLocation = (query) => {
+    let url = `https://us1.locationiq.com/v1/search.php?key=${apiKey}&q=${this.state.searchQuery}&format=json`;
+    axios.get(url)
+      .then(response => {
+        console.log(response);
+        this.setState({
+          lat: response.data[0].lat,
+          lon: response.data[0].lon,
+          name: response.data[0].display_name,
+          error: { status: null, message: null}
+        });
+      })
+      .catch(error => {
+        this.setState ({
+          error: {
+            status: error.response.status,
+            message: error.response.data.message,
+          }
+        })
+        console.log(error);
+      });
+    }
   handleClose = () => this.setState({ show: false});
 
   render(){
     return (
       <>
-        <input style={{marginRight: '1rem', border: '2px solid gray', paddingRight: '2rem'}}onChange={(e) => this.setState({searchQuery: e.target.value })} placeholder="Search by City Name"></input>
-        <button style={{ border: '2px solid gray'}} onClick={this.getLocation}>Explore!</button>  
-        <Card style={{ width: '30rem', margin: '2rem auto', backgroundColor: '#ebdf94'}}>
-          <Card.Title>{this.state.location.display_name}</Card.Title>
-          <Card.Body>
-          <img style={{ width: '100%' }} className='map' src={`https://maps.locationiq.com/v3/staticmap?key=${key}&center=${this.state.location.lat},${this.state.location.lon}&zoom=12&size=480x480`} alt="" />
-          </Card.Body>
-          <Card.Text> City Latitude: {this.state.location.lat} </Card.Text>
-          <Card.Text style={{ paddingBottom: '2rem'}}> City Longitude: {this.state.location.lon}</Card.Text>
-        </Card>
+        
       </>
     )
   }
